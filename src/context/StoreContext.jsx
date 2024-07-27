40d16047f5222f778f80480bb5f78b5f4784a7ef
luongchihao2143 <luongchihao2143@gmail.com>
2024-07-28 00:48:09 +0700

    First commit

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItem, setCartItem] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItem[itemId]) {
+      setCartItem((pre) => ({
+        ...pre,
+        [itemId]: 1,
+      }));
+    } else {
+      setCartItem((pre) => ({
+        ...pre,
+        [itemId]: pre[itemId] + 1,
+      }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItem((pre) => ({ ...pre, [itemId]: pre[itemId] - 1 }));
+  };
+
+  const contextValue = {
+    food_list,
+    cartItem,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+export default StoreContextProvider;
